60574aa241567c9098ea08ee9feaed606a8069b7
FIRMAN PANJI <fpu2468@gmail.com>
2022-04-09 11:23:46 +0700

    Aplikasi Bank Transfer dengan Runtime Config

--- modul7_1302200089/Program.cs
+++ modul7_1302200089/Program.cs
@@ -10,6 +10,131 @@ namespace modul7_1302200089
     {
         static void Main(string[] args)
         {
+            BankTransferConfig bankTFconf = new BankTransferConfig();
+            dynamic config = bankTFconf.ReadJson();
+
+            if (config.lang == "en")
+            {
+                Console.Write("Please insert the amount of money to transfer: ");
+            }
+            else if (config.lang == "id")
+            {
+                Console.Write("Masukkan jumlah uang yang akan di-transfer: ");
+            }
+
+            string uangTFcnfg1 = Console.ReadLine();
+            int cekThreshold = int.Parse(uangTFcnfg1);
+
+            int biayaTF;
+            if (cekThreshold <= (int)config.transfer.threshold)
+            {
+                biayaTF = config.transfer.low_fee;
+            }
+            else
+            {
+                biayaTF = config.transfer.high_fee;
+            }
+
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Transfer fee = " + biayaTF);
+                Console.WriteLine("Total amount = " + (biayaTF + cekThreshold));
+                Console.WriteLine("Select transfer method");
+            }
+            else if (config.lang == "id")
+            {
+                Console.WriteLine("Biaya transfer = " + biayaTF);
+                Console.WriteLine("Total biaya = " + (biayaTF + cekThreshold));
+                Console.WriteLine("Pilih metode transfer");
+            }
+
+            int idx = 0;
+            foreach (var mthd in config.methods)
+            {
+                idx++;
+                Console.WriteLine(idx + ". " + mthd);
+            }
+
+            //Console.WriteLine();
+            string mtdhTF = Console.ReadLine();
+            if (config.lang == "id")
+            {
+                Console.Write("Metode transfer yang dipilih adalah ");
+                if (mtdhTF == "1")
+                {
+                    Console.WriteLine("RTO (real-time)");
+                }
+                else if (mtdhTF == "2")
+                {
+                    Console.WriteLine("SKN");
+                }
+                else if (mtdhTF == "3")
+                {
+                    Console.WriteLine("RTGS");
+                }
+                else if (mtdhTF == "4")
+                {
+                    Console.WriteLine("BI FAST");
+                }
+                else
+                {
+                    Console.WriteLine("Pemilihan metode transfer tidak valid!");
+                }
+            }
+            else if (config.lang == "en")
+            {
+                Console.Write("The method you have chosen is ");
+                if (mtdhTF == "1")
+                {
+                    Console.WriteLine("RTO (real-time)");
+                }
+                else if (mtdhTF == "2")
+                {
+                    Console.WriteLine("SKN");
+                }
+                else if (mtdhTF == "3")
+                {
+                    Console.WriteLine("RTGS");
+                }
+                else if (mtdhTF == "4")
+                {
+                    Console.WriteLine("BI FAST");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer method choice is not valid!");
+                }
+
+            }
+            string konfirmasi;
+            if (config.lang == "en")
+            {
+                Console.WriteLine("Please type '" + config.confirmation.en + "' to confirm the transaction:");
+                konfirmasi = Console.ReadLine();
+                if (konfirmasi == (string)config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled");
+                }
+            }
+            else if (config.lang == "id")
+            {
+                Console.WriteLine("Ketik '" + config.confirmation.id + "' untuk mengkonfirmasi transaksi:");
+                konfirmasi = Console.ReadLine();
+                if (konfirmasi == (string)config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+
+
         }
     }
 }
